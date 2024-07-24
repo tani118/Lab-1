361cd8dc30b996f9ada895d4ca29fb2f144c49a7
Lakshya Bhutani <110778704+tani118@users.noreply.github.com>
2024-07-24 15:06:50 +0530

    Create myProgram.cpp

---
+++ myProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+  cout << "enter number 1 : \n";
+  int x;
+  cin >> x;
+
+  cout << "enter number 2 : \n";
+  int y;
+  cin >> y;
+
+  cout << "sum is " << x + y << "\ndifference is " << x - y << "\nproduct is " << x * y << "\n";
+}
